a61af37525ac7b882e94edc6ba3df57829d199f1
kerimturak <68472927+kerimturak@users.noreply.github.com>
2020-09-30 14:18:59 +0300

    Add files via upload

---
+++ 8-) writing_number_in_0-100.py
@@ -0,0 +1,6 @@
+#Burada i yi 100 den küçük eşit yapmamamızın sebebi i 99 olduğunda döngüye
+# girip döngü içinde 100 olacak ve bir sonraki döngüde i 100<100 sağlamadığından döngüye girmeyecektir
+i = 0
+while i <100:
+    i+=1
+    print(i)
